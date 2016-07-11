44b72d3087d56d32eb2ea57aa4aacca654162867
tomoya-nagatuka <metagurosutomoya@gmail.com>
2016-07-11 15:24:23 +0900

    Add files via upload

---
+++ cgengo4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int num1,num2;
+
+   printf("®”‚P‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B\n");
+
+   scanf("%d",&num1);
+
+   printf("®”‚Q‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B\n");
+
+   scanf("%d",&num2);
+
+   printf("‘«‚µZ‚ÌŒ‹‰Ê‚Í%d‚Å‚·B\n",num1+num2);
+
+   return 0;
+}
